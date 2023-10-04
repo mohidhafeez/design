7eeafd5e38ff9d19ab42208cb18649ffdcbb0014
mohidhafeez <mohidzohaib545@gmail.com>
2023-10-04 16:25:12 +0500

    final

--- lib/delivery_page_view.dart
+++ lib/delivery_page_view.dart
@@ -1,5 +1,5 @@
-import 'package:design/components/tile.dart';
 import 'package:design/utils/size_config.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DeliveryPageView extends StatefulWidget {
@@ -12,13 +12,43 @@ class DeliveryPageView extends StatefulWidget {
 List<String> options = ['option1', 'option2'];
 
 class _DeliveryPageViewState extends State<DeliveryPageView> {
+  String? valueChoose;
+  List listitem = [
+    'Urgent',
+    'Normal',
+  ];
   String currentIndex = options[0];
+  //colors
+  Color container1Color = Colors.transparent;
+  Color container2Color = Colors.transparent;
+  Color container1text = Colors.black;
+  Color container2text = Colors.black;
+
+  void changeColor(int containerIndex) {
+    setState(() {
+      if (containerIndex == 1) {
+        container1Color = Colors.green;
+        container2Color = Colors.transparent;
+        container1text = Colors.white;
+        container2text = Colors.black;
+      } else {
+        container1Color = Colors.transparent;
+        container2Color = Colors.green;
+        container1text = Colors.black;
+        container2text = Colors.white;
+      }
+    });
+  }
+
+//swtich  button
+  bool isSwitched = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink,
+      backgroundColor: Colors.amber[100],
       appBar: AppBar(
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.amber[100],
         title: const Text(
           "Choose Service",
           style: TextStyle(fontSize: 24, color: Colors.black),
@@ -30,7 +60,10 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
               onPressed: () {
                 (Navigator.pop(context));
               },
-              icon: const Icon(Icons.close))
+              icon: const Icon(
+                Icons.close,
+                color: Colors.black38,
+              ))
         ],
       ),
       body: Column(
@@ -56,21 +89,24 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
                       ),
                       const Text(
                         "Select Delivery Type",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
                         textAlign: TextAlign.start,
                       ),
                       SizedBox(
                         height: SizeConfig.heightMultiplier * 3,
                       ),
+                      //lightColor Container
                       Container(
                         height: SizeConfig.heightMultiplier * 18,
                         width: SizeConfig.widthMultiplier * 100,
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
+                          borderRadius: const BorderRadius.only(
                               topLeft: Radius.circular(
                                 20,
                               ),
                               topRight: Radius.circular(20)),
-                          color: Colors.amber[200],
+                          color: Colors.amber[100],
                         ),
                         child: Padding(
                           padding: const EdgeInsets.all(4.0),
@@ -81,10 +117,10 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Container(
+                                  SizedBox(
                                     child: Row(
                                       children: [
-                                        CircleAvatar(
+                                        const CircleAvatar(
                                           backgroundColor: Colors.green,
                                           child: Icon(Icons.flight_outlined),
                                         ),
@@ -95,7 +131,7 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
                                           crossAxisAlignment:
                                               CrossAxisAlignment.start,
                                           children: [
-                                            Text(
+                                            const Text(
                                               "Air Freight",
                                               style: TextStyle(
                                                   fontWeight: FontWeight.w500),
@@ -105,7 +141,7 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
                                                   SizeConfig.widthMultiplier *
                                                       2,
                                             ),
-                                            Text(
+                                            const Text(
                                               "Reliable and Hassle Free",
                                               style: TextStyle(
                                                   color: Colors.green),
@@ -116,7 +152,7 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
                                     ),
                                   ),
                                   Radio(
-                                      fillColor: MaterialStatePropertyAll(
+                                      fillColor: const MaterialStatePropertyAll(
                                           Colors.orange),
                                       value: options[0],
                                       groupValue: currentIndex,
@@ -134,10 +170,10 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Container(
+                                  SizedBox(
                                     child: Row(
                                       children: [
-                                        CircleAvatar(
+                                        const CircleAvatar(
                                           backgroundColor: Colors.green,
                                           child: Icon(
                                               Icons.directions_boat_outlined),
@@ -149,7 +185,7 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
                                           crossAxisAlignment:
                                               CrossAxisAlignment.start,
                                           children: [
-                                            Text(
+                                            const Text(
                                               "Ocean Freight",
                                               style: TextStyle(
                                                   fontWeight: FontWeight.w500),
@@ -159,7 +195,7 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
                                                   SizeConfig.widthMultiplier *
                                                       2,
                                             ),
-                                            Text(
+                                            const Text(
                                               "Reliable and Hassle Free",
                                               style: TextStyle(
                                                   color: Colors.green),
@@ -170,7 +206,7 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
                                     ),
                                   ),
                                   Radio(
-                                      fillColor: MaterialStatePropertyAll(
+                                      fillColor: const MaterialStatePropertyAll(
                                           Colors.orange),
                                       value: options[1],
                                       groupValue: currentIndex,
@@ -185,17 +221,199 @@ class _DeliveryPageViewState extends State<DeliveryPageView> {
                           ]),
                         ),
                       ),
+                      //bottom Container
                       Container(
                         height: SizeConfig.heightMultiplier * 7,
                         width: SizeConfig.widthMultiplier * 100,
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
+                          borderRadius: const BorderRadius.only(
                               bottomLeft: Radius.circular(
                                 20,
                               ),
                               bottomRight: Radius.circular(20)),
-                          color: Colors.amber,
+                          color: Colors.amber[200],
                         ),
+                        //dropdown button
+                        child: Center(
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 50.0),
+                            child: DropdownButton(
+                              icon: const Icon(
+                                CupertinoIcons.arrow_down,
+                                color: Colors.orange,
+                              ),
+                              hint: const Text(
+                                "View All type",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.orange,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              value: valueChoose,
+                              onChanged: (newValue) {
+                                setState(() {
+                                  valueChoose = newValue.toString();
+                                });
+                              },
+                              items: listitem.map((valueItem) {
+                                return DropdownMenuItem(
+                                  value: valueItem,
+                                  child: Text(valueItem),
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ),
+                      ),
+//select container
+                      SizedBox(
+                        height: SizeConfig.heightMultiplier * 2,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              changeColor(1);
+                            },
+                            child: Container(
+                              height: SizeConfig.heightMultiplier * 20,
+                              width: SizeConfig.widthMultiplier * 42,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(color: Colors.black26),
+                                color: container1Color,
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const CircleAvatar(
+                                    child: Icon(Icons.flight_outlined),
+                                  ),
+                                  SizedBox(
+                                    height: SizeConfig.heightMultiplier * 2,
+                                  ),
+                                  Text(
+                                    "Regular",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: container1text),
+                                  ),
+                                  SizedBox(
+                                    height: SizeConfig.heightMultiplier * 2,
+                                  ),
+                                  Text(
+                                    "Super Fast and\n Safe Delivery",
+                                    style: TextStyle(color: container1text),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              changeColor(2);
+                            },
+                            child: Container(
+                              height: SizeConfig.heightMultiplier * 20,
+                              width: SizeConfig.widthMultiplier * 42,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(color: Colors.black26),
+                                color: container2Color,
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const CircleAvatar(
+                                    child: Icon(Icons.rocket_outlined),
+                                  ),
+                                  SizedBox(
+                                    height: SizeConfig.heightMultiplier * 2,
+                                  ),
+                                  Text(
+                                    "Lightning",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: container2text),
+                                  ),
+                                  SizedBox(
+                                    height: SizeConfig.heightMultiplier * 2,
+                                  ),
+                                  Text(
+                                    "Fast , Silent and\n Safe Delivery",
+                                    style: TextStyle(color: container2text),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      //tile
+                      SizedBox(
+                        height: SizeConfig.heightMultiplier * 2,
+                      ),
+                      Container(
+                        height: SizeConfig.heightMultiplier * 10,
+                        width: SizeConfig.widthMultiplier * 100,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.black38)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      "Parcel Guranateed!",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: SizeConfig.heightMultiplier * 1,
+                                    ),
+                                    const Text(
+                                      "More guaranteed up to 74% safe",
+                                      style: TextStyle(color: Colors.black54),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              //swtich button
+                              CupertinoSwitch(
+                                activeColor: Colors.green,
+                                value: isSwitched,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isSwitched = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.heightMultiplier * 3,
+                      ),
+                      Container(
+                        height: SizeConfig.heightMultiplier * 6,
+                        width: SizeConfig.widthMultiplier * 100,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: const Color.fromARGB(255, 255, 111, 0)),
+                        child: const Center(
+                            child: Text("Calculate",
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white))),
                       )
                     ],
                   ),
